edf29a02fb7a22d58e2aa510b1140df8b56c0f1c
6ickomod3 <jidai2014@gmail.com>
2025-11-28 16:06:55 -0800

    Initial Commit

---
+++ PersonalTrainerApp/PersonalTrainerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalTrainerAppApp.swift
+//  PersonalTrainerApp
+//
+//  Created by Ji Dai on 11/28/25.
+//
+
+import SwiftUI
+
+@main
+struct PersonalTrainerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PersonalTrainerAppTests/PersonalTrainerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalTrainerAppTests.swift
+//  PersonalTrainerAppTests
+//
+//  Created by Ji Dai on 11/28/25.
+//
+
+import Testing
+@testable import PersonalTrainerApp
+
+struct PersonalTrainerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
